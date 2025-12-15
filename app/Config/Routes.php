742e2c639f92de92819cce5748fb096316782924
Armando <L221061091@mulege.tecnm.mx>
2025-12-14 20:06:43 -0700

    se agregaron las rutas

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/auth/login', 'Auth::login');
+$routes->get('/auth/register', 'Auth::register');
+$routes->post('/auth/loginPost', 'Auth::loginPost');
+$routes->post('/auth/registerPost', 'Auth::registerPost');
