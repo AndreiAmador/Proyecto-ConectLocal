d88491eef0489f009a5f9de4e5ce8a4580ecf5e3
Armando <L221061091@mulege.tecnm.mx>
2025-12-14 21:31:10 -0700

    se agrego la bienvenida del home y la ruta

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,6 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/home', 'Home::index');
 $routes->get('/auth/login', 'Auth::login');
 $routes->get('/auth/register', 'Auth::register');
 $routes->post('/auth/loginPost', 'Auth::loginPost');
