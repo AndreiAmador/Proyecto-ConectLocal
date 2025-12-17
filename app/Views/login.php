0d3981ae467aff63f3684eec64d00dafce08a56d
André San Arce <l221061048@mulege.tecnm.mx>
2025-12-16 17:32:34 -0700

    Diseño login

--- app/Views/login.php
+++ app/Views/login.php
@@ -1,28 +1,36 @@
+<?= $this->extend('layouts/main') ?>
+
+<?= $this->section('content') ?>
+
+<h2>Iniciar sesión</h2>
+
 <?php if (session()->getFlashdata('error')): ?>
     <div class="alert alert-danger">
         <?= session()->getFlashdata('error'); ?>
     </div>
 <?php endif; ?>
 
-<form action="/auth/loginPost" method="post">
-    <div>
+<form action="/auth/loginPost" method="post" class="form">
+
+    <div class="form-group">
         <label for="username">Usuario</label>
         <input type="text" name="username" id="username" required>
     </div>
-    <div>
+
+    <div class="form-group">
         <label for="password">Contraseña</label>
         <input type="password" name="password" id="password" required>
     </div>
-    <button type="submit">Iniciar sesión</button>
+
+    <button type="submit" class="btn btn-primary">
+        Iniciar sesión
+    </button>
+
 </form>
 
-<p>¿No tienes una cuenta? <a href="/auth/register">Regístrate aquí</a></p>
-<?= $this->extend('layouts/main') ?>
-
-<?= $this->section('content') ?>
-
-<h2>Iniciar sesión</h2>
-<input type="text">
-<button class="btn-primary">Login</button>
+<p class="mt">
+    ¿No tienes una cuenta?
+    <a href="/auth/register">Regístrate aquí</a>
+</p>
 
 <?= $this->endSection() ?>
